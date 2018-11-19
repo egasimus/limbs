d30f44de204e1b2529632bd07da683787c8f9009
Adam Avramov <adama@aiidatapro.com>
2018-11-19 19:58:21 +0200

    update eventemitter

--- packages/limbs-eventemitter/index.js
+++ packages/limbs-eventemitter/index.js
@@ -1,15 +1,15 @@
 module.exports = function EventsTrait () {
   var configs = Array.prototype.slice.call(arguments)
-  return function Events (core) {
-    if (!core.hasTrait('Private')) require('limbs-private')()(core)
-    core.private.events = new (require('rxjs').Subject)()
-    Object.defineProperty(core.public, 'events',
-      { enumerable: true, get: function () { return core.private.events } })
+  return function Events (add) {
+    console.log(this)
+    this.events = new (require('rxjs').Subject)()
+    Object.defineProperty(this.public, 'events',
+      { enumerable: true, get: function () { return this.events }.bind(this) })
     ;['emit', 'on', 'off', 'once', 'onAny'].forEach(function (method) {
-      Object.defineProperty(core.public, method,
+      Object.defineProperty(this.public, method,
         { enumerable: true, get: function () {
           return function () {
-            return require('./methods')[method].apply(core, arguments) } } }) })
+            return require('./methods')[method].apply(this, arguments) } } }) }.bind(this))
     configs.forEach(function (config) {
       if (!config) return
       var boundSpec = config(actor)
--- packages/limbs-eventemitter/methods.js
+++ packages/limbs-eventemitter/methods.js
@@ -3,16 +3,16 @@ var filter = require('rxjs/operators').filter
 module.exports = {
 
   emit: function () { // send [<event>, <arg1>, ...<argN>] down the event stream
-    this.private.events.next(Array.prototype.slice.call(arguments)) },
+    this.events.next(Array.prototype.slice.call(arguments)) },
 
   on: function (predicate, handler) {
-    return this.private.events
+    return this.events
       .pipe(filter(function (event) { return event[0] === predicate }))
       .subscribe(handler) },
 
   once: function (predicate, handler) {
     var subscription
-    return subscription = this.private.events
+    return subscription = this.events
       .pipe(filter(function (event) { return event[0] === predicate }))
       .subscribe(event=>{
         // TODO: switch the order of these to implement 'DoNotUnsubscribeOnThrow'
