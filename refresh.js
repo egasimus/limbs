2762c36d9aa67ec88aa3b03d0be74a9599261b1e
Adam Avramov <adama@aiidatapro.com>
2018-11-24 21:48:35 +0200

    full-app hot reload from module

--- refresh.js
+++ refresh.js
@@ -1,8 +1,27 @@
 module.exports = function Refresh (state = {}) {
+
   if (state.Refresh) state.Refresh.unsubscribe()
+
   state.Refresh = state.Events.on('FileChanged', ([_, uri]) => {
+
     const location = state.Files.resolve(uri)
     if (Object.keys(require.cache).indexOf(location) == -1) return
-    require('clear-module')(location)
-    state.Events.emit('Refreshed', location) })
-  return state }
+
+    const refreshList = []
+
+    const add = location => {
+      if (refreshList.indexOf(location) === -1) refreshList.push(location)
+      if (state.Deps[location]) [...state.Deps[location]].forEach(dep=>add(dep)) }
+
+    add(location)
+
+    refreshList.forEach(location=>{
+      require('clear-module')(location)
+      delete state.Deps[location]
+      state.Events.emit('Refreshed', location) })
+  
+  })
+
+  return state
+
+}
